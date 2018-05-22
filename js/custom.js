d51443f399de7d61f7e4c25ad7c4f0a8764e80ee
Marius <marius@by-mc.co>
2018-05-22 13:16:41 +0200

    1st Upload
    Upload

---
+++ js/custom.js
@@ -0,0 +1,216 @@
+var dictionary = {
+		//MENU
+			'menuAbout' : {
+				'en': 'ABOUT',
+				'cn': '关于',
+			},
+			'menuFeatures' : {
+				'en': 'FEATURES',
+				'cn': '特性',
+			},
+			'menuHowToScan' : {
+				'en': 'HOW TO SCAN',
+				'cn': '扫描过程',
+			},
+			'menuKiosk' : {
+				'en': 'OUR KIOSK',
+				'cn': '产品',
+			},
+			'menuApp' : {
+				'en': 'APP',
+				'cn': '应用',
+			},
+			'menuNews' : {
+				'en': 'NEWS',
+				'cn': '新闻',
+			},
+			'menuContact' : {
+				'en': 'CONTACT US',
+				'cn': '联系',
+			},
+			'menuBlog' : {
+				'en': 'OUR BLOG',
+				'cn': '博客',
+			},		
+
+		//ABOUT US SECTION
+			'aboutTitle' : {
+				'en': 'Your Body in 3D',
+				'cn': 'Your Body in 3D',
+			},
+			'aboutDescription': {
+				'en': 'Shapewatch is a next generation body monitoring technology that allows users to scan themselves, visualise their body shape and measurements in 3D, and track their progress towards their fitness goals.',
+				'cn': 'Shapewatch是全新一代3D体型分析仪。用户可通过简单的操作实现体型扫描，3D轮廓查看，肢体数据测量，体型变化跟踪，趋势预测等。',
+			},
+
+		// FEATURE SECTION
+			'featuresTitle' : {
+				'en': 'Motivation <br>through Visualisation',
+				'cn': '可视化激励',
+			},
+			'featuresDescription': {
+				'en': 'Shapewatch’s unique kiosk and app experience allows users to undertake their own scans in less than 20 seconds, in a discreet, non-invasive manner, and produce impactful visualisations of their body in 3D.<br><br>Continued use of Shapewatch allows users to visualise change in their body shape and variations in body measurements, and maintain their motivation towards their fitness goal.',
+				'cn': 'Shapewatch能在20秒内完成体型扫描过程，并立即显示出动态3D模型。<br><br>以谨慎的非侵入方式',
+			},
+			// FEATURE ONE
+			'featureOneTitle' : {
+				'en': 'Scan your body',
+				'cn': '扫描您的体型',
+			},
+			'featureOneDesc' : {
+				'en': 'Quick QR access<br> Simple instructions',
+				'cn': '快速QR码识别<br> 简单操作',
+			},
+			// FEATURE TWO
+			'featureTwoTitle' : {
+				'en': 'Visualise your shape',
+				'cn': '查看您的体型',
+			},
+			'featureTwoDesc' : {
+				'en': 'Intuitive App<br> 3D Animated Avatars',
+				'cn': '简易直观的应用程序<br> 动态3D体型建模',
+			},
+			// FEATURE THREE
+			'featureThreeTitle' : {
+				'en': 'Track your change',
+				'cn': '跟踪您的变化',
+			},
+			'featureThreeDesc' : {
+				'en': 'Monitor timeline progress<br> Measurements in motion',
+				'cn': '时间轴记录进程<br> 过程数据测量',
+			},
+		// VIDEO SECTION
+			'videoTitle' : {
+				'en': 'Ready - Scan - Shape',
+				'cn': '准备 – 扫描 – 完成',
+			},
+			'videoDesc' : {
+				'en': '- Just tap "+" in the Shapewatch App<br><br>- Show QR code to the Shapewatch Kiosk<br><br>- Strike a pose - from the front and side<br><br>- Scan is done - you`re set and ready to get into shape!',
+				'cn': '- 点击Shapewatch应用中的“+”<br><br> 分析仪识别用户QR码<br><br>- 扫描姿势 – 正面和侧面<br><br>- 扫描完成 – 努力变成“有型”的你！',
+			},
+		// KIOSK SECTION
+			'kioskTitle' : {
+				'en': 'Shapewatch kiosk',
+				'cn': 'Shapewatch“扫描桩”',
+			},
+			'kioskDesc' : {
+				'en': 'Simple Aesthetic. Personal Experience. Accurate Results.<br><br> The tubular Shapewatch kiosk has been thoughtfully designed with the user experience as its focus.<br><br> Shapewatch’s console display provides simple instructions to guide users through the scanning process',
+				'cn': '极简美学，注重体验，准确测量。<br><br> 简约的桩式外形强调设计注重用户体验，<br><br> 分析仪通过直观的图形提示引导整个扫描过程并实现人机零接触。',
+			},
+			// KIOSK FEATURE ONE
+			'kioskFeatureOneTitle' : {
+				'en': 'Convenience',
+				'cn': '便捷',
+			},
+			'kioskFeatureOneDesc' : {
+				'en': 'Shapewatch is a user-activated experience',
+				'cn': '用户驱动的应用体验',
+			},
+			// KIOSK FEATURE TWO
+			'kioskFeatureTwoTitle' : {
+				'en': 'Cutting-Edge',
+				'cn': '前沿技术',
+			},
+			'kioskFeatureTwoDesc' : {
+				'en': '3D scanning technology using advanced depth sensors and algorithms',
+				'cn': '先进的深度传感器技术',
+			},
+			// KIOSK FEATURE THREE
+			'kioskFeatureThreeTitle' : {
+				'en': 'Clean Design',
+				'cn': '简约设计',
+			},
+			'kioskFeatureThreeDesc' : {
+				'en': 'Carefully designed with the user in mind',
+				'cn': '专注用户体验的设计',
+			},				
+		// APP SECTION
+			'appTitle' : {
+				'en': 'Shapewatch application',
+				'cn': 'Shapewatch应用',
+			},
+			'appDesc' : {
+				'en': 'The Shapewatch app provides detailed Avatar representations of the body, which combined with body measurements and health data provide a highly visual overview to assist on your fitness goals. Intelligent software predicts future changes, enhancing Shapewatch’s motivational benefits, and keeping you engaged on your fitness journey.',
+				'cn': 'Shapewatch应用呈现的细节人体3D动态模型包含体型测量数据及健康数据。这些数据可为您制定健身计划提供高度可视化的参考。 智能预测未来体型变化，加强健身人群的主动锻炼意识，引导完成健身计划',
+			},
+			'appDl' : {
+				'en': 'Available to download now.',
+				'cn': '现在就可以下载。',
+			},
+		// NEWS SECTION
+			'newsTitle' : {
+				'en': 'Latest News',
+				'cn': '最近新闻',
+			},
+			'newsDesc' : {
+				'en': 'The Shapewatch team keep in touch with the world of fitness, body assessment, and general health so bookmark us on your browser to stay connected.',
+				'cn': 'Shapewatch团队愿意和健身俱乐部，健身者以及关注身体健康的所有人保持联系，敬请收藏我们的网站！',
+			},
+			//NEWS ONE
+			'newsOneTitle' : {
+				'en': 'Shapewatch Showcasing at IWF Shanghai 2018',
+				'cn': 'Shapewatch在中国上海IWF 2018展出',
+			},
+			'newsOneDesc' : {
+				'en': 'Shapewatch, the next-generation body monitoring technology company, is showcasing its latest 3D body experience at IWF Shanghai, the most premium fitness event in China...',
+				'cn': 'Shapewatch, the next-generation body monitoring technology company, is showcasing its latest 3D body experience at IWF Shanghai, the most premium fitness event in China...',
+			},
+			//NEWS TWO
+			'newsTwoTitle' : {
+				'en': 'Shapewatch present at the IHRSA 2018 in San Diego, U.S.',
+				'cn': 'Shapewatch在圣地亚哥IHRSA 2018展出',
+			},
+			'newsTwoDesc' : {
+				'en': 'The IHRSA International Convention & Trade Show is world-renowned for its vibrant trade show, motivational keynote speakers, expert educational sessions, and buzzing networking events...',
+				'cn': 'The IHRSA International Convention & Trade Show is world-renowned for its vibrant trade show, motivational keynote speakers, expert educational sessions, and buzzing networking events...',
+			},
+
+			'newsReadMore' : {
+				'en': 'Read More',
+				'cn': '阅读更多',
+			},
+		// CONTACT SECTION
+			'contactTitle' : {
+				'en': 'The Shapewatch team would love to hear from any users so please feel free to stay in touch!',
+				'cn': 'Shapewatch 团队希望和您取得联系！',
+			},
+			'contactButtonTwo' : {
+				'en': 'General enquires',
+				'cn': '产品咨询',
+			},
+			'contactButtonTitle' : {
+				'en': 'Our blog',
+				'cn': '博客',
+			},
+			'contactButtonThree' : {
+				'en': 'Check us out',
+				'cn': '了解更多',
+			},
+
+};
+var langs = ['en', 'cn',];
+var current_lang_index = 0;
+var current_lang = langs[current_lang_index];
+
+window.change_lang = function() {
+	current_lang_index = ++current_lang_index % 2;
+	current_lang = langs[current_lang_index];
+	if (current_lang == 'cn'){
+		$('.changeLang a img.cn-flag').hide();
+		$('.changeLang a img.us-flag').fadeIn();			
+	}else{
+		$('.changeLang a img.us-flag').hide();
+		$('.changeLang a img.cn-flag').fadeIn();		
+	}
+	translate();
+}
+
+function translate() {
+
+	$("[data-translate]").each(function(){
+		var key = $(this).data('translate');
+		$(this).hide().html(dictionary[key][current_lang] || "N/A").fadeIn();		
+	});
+}
+
+translate();
